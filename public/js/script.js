3aa3d4625c37db7c19a5b019596cfbf5de51e2a7
Kevin Kipkkoech <Kevin Kipkkoech>
2017-06-20 03:23:27 +0300

    Setting up the map on the front page

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 0.3031, lng: 36.0800},
+        scrollwheel: false,
+        zoom: 8
+    });
+
+});
